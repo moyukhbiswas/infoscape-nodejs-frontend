b997d5088fd7866a44db0f36bc990916e8159d2d
Moyukh Biswas <mobisw@microsoft.com>
2016-07-26 11:28:53 +0530

    port

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.use(express.static('public'));
 
+var port = process.env.PORT || 3000;
+
 app.get('/',express.static('public'));
 
 app.get('/someapicallterminal', function(req, res) {
 	console.log('API CALL HERE');
 	res.end('API Only!');
 });
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
 });
